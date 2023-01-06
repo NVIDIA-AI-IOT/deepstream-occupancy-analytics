01350aebec184bb3ad0db588b5196c319c959ac8
david <david@rai.dscloud.me>
2023-01-06 10:44:40 +0000

    update change_file

--- change_file.sh
+++ change_file.sh
@@ -2,6 +2,6 @@
 cp changefile/nvdsmeta_schema.h  /opt/nvidia/deepstream/deepstream/sources/includes/nvdsmeta_schema.h
 cd config && ./model.sh
 cd /opt/nvidia/deepstream/deepstream/sources/libs/nvmsgconv && make && cp libnvds_msgconv.so /opt/nvidia/deepstream/deepstream/lib/libnvds_msgconv.so
-cd /opt/nvidia/deepstream/deepstream/sources/apps/sample_apps/deepstream-occupancy-analytics/ && \
-CUDA_VER=11.4 make && \
-./deepstream-test5-analytics -c config/test5_config_file_src_infer_tlt.txt
+# cd /opt/nvidia/deepstream/deepstream/sources/apps/sample_apps/deepstream-occupancy-analytics/ && \
+# CUDA_VER=11.4 make && \
+# ./deepstream-test5-analytics -c config/test5_config_file_src_infer_tlt.txt
