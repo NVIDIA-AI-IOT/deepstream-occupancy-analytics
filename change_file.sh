240a823a4febf15cf8d73ffb4cc7fde017055c8c
david <david@rai.dscloud.me>
2023-01-06 10:30:10 +0000

    update

--- change_file.sh
+++ change_file.sh
@@ -1,3 +1,3 @@
-git clone git@github.com:ddasdkimo/deepstream-occupancy-analytics.git -b pc
+# git clone git@github.com:ddasdkimo/deepstream-occupancy-analytics.git -b pc
 cp changefile/nvdsmeta_schema.h  /opt/nvidia/deepstream/deepstream/sources/includes/nvdsmeta_schema.h
 cd /opt/nvidia/deepstream/deepstream/sources/libs/nvmsgconv && make && cp libnvds_msgconv.so /opt/nvidia/deepstream/deepstream/lib/libnvds_msgconv.so
